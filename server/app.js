b7ab6652e6dbe35d32ff66bded8bdda84a62a96d
Alice Chen <alicejychen15@gmail.com>
2018-02-01 02:05:51 -0500

    set up the server

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const path = require('path')
+const PORT = process.env.PORT || 8080
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use('/', express.static(path.join(__dirname, '../public')))
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message || 'Internal server error')
+})
+app.listen(PORT, () => console.log('listening on port ', PORT))
