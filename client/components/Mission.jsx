bbfaf06b56abac2ff39706d7f58d2dd3197971ad
Alice Chen <alicejychen15@gmail.com>
2018-02-05 17:43:36 -0500

    basic style mission component

--- client/components/Mission.jsx
+++ client/components/Mission.jsx
@@ -8,15 +8,15 @@ export default class Mission extends Component {
   render () {
     return (
       <div className="info">
+        <div className="col"> 
+          <h1 className="mission">MISSION FOR INNOVATION</h1>
+        </div>
         <div className="col-md-6 offset-md-3"> 
           <ul className="info--list "> 
             <li><b>Administer</b> daily oral darapladib (160 mg) as  done in previous cardiovascular and DME trials</li>
-
             <li><b>Enroll</b> DME patients with baseline vision worse  than 20/40 (rather than 20/32)
             Same criteria as Lucentis Phase-3 Trial</li>
-
             <li><b>Enroll</b> DME patients who respond poorly to  anti-VEGF therapy (50% of DME patients)</li>
-
           </ul>
         </div>
       </div>
